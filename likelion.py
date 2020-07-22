628b7d8fbdcea338259d6cf7903cb35f361b5d15
Hajun <gkwns5791@namver.com>
2020-07-22 20:02:47 +0900

    kwonhajun

--- likelion.py
+++ likelion.py
@@ -1,6 +1,7 @@
-# Github Organization을 통한 협업 실습
+﻿# Github Organization을 통한 협업 실습
 
 print('안녕하세요 이승준입니다.')
 print('안녕하세요 주은지입니다.')
 print('안녕하세요 박병호입니다.')
 print('안녕하세요 장혜린입니다.')
+print("안녕하세요 권하준입니다.")
